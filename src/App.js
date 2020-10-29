46a54d7c5ff514a2a31fad3d848f55aa8df6b6b0
sedx876 <swatkins876@gmail.com>
2020-10-29 08:02:50 -0400

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 
 import React, { Component } from 'react';
+import CommentBox from './components/CommentBox';
+import Comments from './components/Comments';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAddComment = this.handleAddComment.bind(this);
+
+    this.state = {
+      comments: []
+    }
+  }
+
+  handleAddComment(comment) {
+    this.setState(prevState => {
+      return {
+        comments: prevState.comments.concat(comment)
+      };
+    });
+  }
+
+  componentDidMount() {
+    /*global Ably*/
+    const channel = Ably.channels.get('comments');
+  
+    channel.attach();
+      channel.once('attached', () => {
+        channel.history((err, page) => {
+          // create a new array with comments only in an reversed order (i.e old to new)
+          const comments = Array.from(page.items, item => item.data)
+  
+          this.setState({ comments });
+  
+          channel.subscribe((msg) => {
+            const commentObject = msg.data;
+            this.handleAddComment(commentObject);
+          })
+        });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <CommentBox handleAddComment={this.handleAddComment} />
+              <Comments comments={this.state.comments} />
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -3,23 +3,22 @@ import React, { Component } from 'react';
 class CommentBox extends Component {
   constructor(props) {
     super(props);
+    this.addComment = this.addComment.bind(this);
   }
 
   addComment(e) {
     // Prevent the default behaviour of form submit
     e.preventDefault();
-  
+
     // Get the value of the comment box
     // and make sure it not some empty strings
     const comment = e.target.elements.comment.value.trim();
     const name = e.target.elements.name.value.trim();
-  
+
     // Make sure name and comment boxes are filled
     if (name && comment) {
       const commentObject = { name, comment };
-  
-      this.props.handleAddComment(commentObject);
-  
+
       // Publish comment
       /*global Ably*/
       const channel = Ably.channels.get('comments');
@@ -28,7 +27,7 @@ class CommentBox extends Component {
           console.log('Unable to publish message; err = ' + err.message);
         }
       });
-  
+
       // Clear input fields
       e.target.elements.comment.value = '';
       e.target.elements.name.value = '';
